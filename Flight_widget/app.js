9e9de4e6062e8e01b37d211317c27b04f4e34330
Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
2022-07-27 14:05:39 -0700

    Create Flight widget

---
+++ Flight_widget/app.js
@@ -0,0 +1,39 @@
+const tableBody = document.getElementById('table-body')
+
+let flights = [
+    {
+        time: "8:11",
+        destination: "OMAN",
+        flight: "0X 203",
+        gate: "A 01",
+        remarks: "ON TIME"
+    },
+    {
+        time: "12:39",
+        destination: "LONDON",
+        flight: "CL 320",
+        gate: "C 31",
+        remarks: "CANCELLED"
+    },
+    {
+        time: "13:21",
+        destination: "OMAN",
+        flight: "0X 203",
+        gate: "A 01",
+        remarks: "ON TIME"
+    },
+    {
+        time: "8:11",
+        destination: "OMAN",
+        flight: "0X 203",
+        gate: "A 01",
+        remarks: "ON TIME"
+    },
+    {
+        time: "8:11",
+        destination: "OMAN",
+        flight: "0X 203",
+        gate: "A 01",
+        remarks: "ON TIME"
+    }
+]
