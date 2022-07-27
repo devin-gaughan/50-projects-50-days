d44a56c25369a8d102d10d98ec945f18da04b636
Devin Gaughan <12529122+Auraeon@users.noreply.github.com>
2022-07-27 15:59:40 -0700

    Update Flight_widget
    Added table of flights

--- Flight_widget/app.js
+++ Flight_widget/app.js
@@ -17,23 +17,33 @@ let flights = [
     },
     {
         time: "13:21",
-        destination: "OMAN",
-        flight: "0X 203",
-        gate: "A 01",
+        destination: "DUBAI",
+        flight: "DBX 201",
+        gate: "A 19",
+        remarks: "CANCELLED"
+    },
+    {
+        time: "14:01",
+        destination: "FRANKFURT",
+        flight: "FR 402",
+        gate: "B 02",
         remarks: "ON TIME"
     },
     {
-        time: "8:11",
-        destination: "OMAN",
-        flight: "0X 203",
-        gate: "A 01",
-        remarks: "ON TIME"
-    },
-    {
-        time: "8:11",
-        destination: "OMAN",
-        flight: "0X 203",
-        gate: "A 01",
-        remarks: "ON TIME"
+        time: "15:22",
+        destination: "TOKYO",
+        flight: "TK 211",
+        gate: "A 32",
+        remarks: "DELAYED"
     }
 ]
+
+function populateTable() {
+    for (const flight of flights) {
+        const tableRow = document.createElement('tr')
+        for(const flightDetail in flight) {
+            const tableCell = document.createElement("td")
+        }
+        tableBody.append(tableRow)
+    }
+}
