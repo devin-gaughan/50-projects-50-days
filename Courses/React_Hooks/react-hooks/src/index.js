d93c73fb2823f6eab56094faff29b2acb1feaa91
Devin Gaughan <12529122+devin-gaughan@users.noreply.github.com>
2025-01-24 22:11:32 +0700

    ok

--- Courses/React_Hooks/react-hooks/src/index.js
+++ Courses/React_Hooks/react-hooks/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
-const people = ["Devin", "Danielle", "Douglas"];
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="Devin" />
+    <App />
   </React.StrictMode>
 );
 
